e76f085db5f1104af79a2f1f44a1f2b759d0eea6
Abduulayev Turan <99797460+AbdullayevTuran-027@users.noreply.github.com>
2022-06-06 13:38:56 +0400

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using FrontToBack.DAL;
+using FrontToBack.Models;
+using FrontToBack.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FrontToBack.Controllers
+{
+    public class HomeController : Controller
+    {
+        private AppDbContext _context;
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            HomeVm homeVm = new HomeVm();   
+            homeVm.slider = _context.Sliders.ToList();
+            homeVm.pageIntro = _context.PageIntros.FirstOrDefault();
+            homeVm.Products= _context.Products.Include(p=>p.Category).ToList();
+            homeVm.Categories= _context.Categories.ToList();
+            
+            
+            return View(homeVm);
+        }
+    }
+}
